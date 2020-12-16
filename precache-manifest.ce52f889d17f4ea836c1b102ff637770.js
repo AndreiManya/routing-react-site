552615e4df540a76bb234fe4157acc2a8f67f211
AndreiManya <1234and95@gmail.com>
2020-12-16 16:08:23 +0300

    Updates

---
+++ precache-manifest.ce52f889d17f4ea836c1b102ff637770.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83464054607bbefc72a6c9ab764ae04f",
+    "url": "/routing-react-site/index.html"
+  },
+  {
+    "revision": "bffb5802ec924f6e23ce",
+    "url": "/routing-react-site/static/css/main.4eaaab61.chunk.css"
+  },
+  {
+    "revision": "520f6d9c134bce6b77c6",
+    "url": "/routing-react-site/static/js/2.680f244f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/routing-react-site/static/js/2.680f244f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bffb5802ec924f6e23ce",
+    "url": "/routing-react-site/static/js/main.17d59046.chunk.js"
+  },
+  {
+    "revision": "08390525fa4a8ecb9ee6",
+    "url": "/routing-react-site/static/js/runtime-main.c5a5402d.js"
+  },
+  {
+    "revision": "b2141cb2e7937369475ea6a7bad716f9",
+    "url": "/routing-react-site/static/media/bg.b2141cb2.png"
+  }
+]);
